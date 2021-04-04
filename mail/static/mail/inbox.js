380493fd38957b91dcdb4d51a217b28a48619a1b
Diego Giraldo <giraldiego@gmail.com>
2021-04-04 15:57:47 -0500

    Clean submitHandler function

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -26,43 +26,43 @@ function compose_email() {
 }
 
 function submitHandler(ev) {
-  console.log("submit button was pressed");
+  
   // Get data from form:
   const recipients = document.querySelector("#compose-recipients").value;
-  console.log(recipients);
   const subject = document.querySelector("#compose-subject").value;
-  console.log(subject);
   const body = document.querySelector("#compose-body").value;
-  console.log(body);
 
+  // build email object
+  const email = {
+    recipients: recipients,
+    subject: subject,
+    body: body
+  };
 
   // Validate data
+  // TODO
 
   // Make request to server
-    // fetch('/emails', {
-    //   method: 'POST',
-    //   body: JSON.stringify({
-    //       recipients: 'user@hogwarts.ac.uk',
-    //       subject: 'Test mail #',
-    //       body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae velit recusandae aperiam nostrum sequi mollitia exercitationem natus ex ullam placeat!'
-    //   })
-    // })
-    // .then(response => response.json())
-    // .then(result => {
-    //     // Print result
-    //     console.log(result);
-    // });
-  // alert("submit button pressed!");
-  // Disable default submit behavior
-  ev.preventDefault();
-  return false;
-}
+  fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify(email)
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
 
-function load_mailbox(mailbox) {
+    // Disable default submit behavior
+    ev.preventDefault();
+    return false;
+  }
   
-  // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
-  document.querySelector('#compose-view').style.display = 'none';
+  function load_mailbox(mailbox) {
+    
+    // Show the mailbox and hide other views
+    document.querySelector('#emails-view').style.display = 'block';
+    document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
