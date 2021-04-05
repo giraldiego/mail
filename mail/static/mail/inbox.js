4622d901141306f07620692e10cda5dac5aefe76
Diego Giraldo <giraldiego@gmail.com>
2021-04-04 21:02:39 -0500

    Render mailboxes elements

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -59,11 +59,62 @@ function submitHandler(ev) {
   }
   
   function load_mailbox(mailbox) {
-    
-    // Show the mailbox and hide other views
-    document.querySelector('#emails-view').style.display = 'block';
-    document.querySelector('#compose-view').style.display = 'none';
+
+  // Save #emails-view node ref
+  const emails_view = document.querySelector("#emails-view");
+  
+  // Show the mailbox and hide other views
+  emails_view.style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+
+  // Get emails from server
+  fetch('/emails/' + mailbox)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      // Create parent container div
+      const container = document.createElement("div");
+
+      emails.forEach(email => {
+
+        // Create email row div 
+        const row = document.createElement("div");
+        row.classList.add("email-item");
+        if (!email.read) row.classList.add("email-item-unread");
+
+        // Add desired fields to shown
+        ["sender", "subject", "timestamp"].forEach((e) => {
+          const item = document.createElement("span");
+          item.innerHTML = email[e];
+          row.append(item);
+        });   
+
+        // Add email row to parent container
+        container.append(row);
+      });
+
+      // Add list of emails to emails-view
+      emails_view.append(container);
+
+      // Each email should then be rendered in its own box (e.g. as a <div> with a border) that displays who the email is from, what the subject line is, and the timestamp of the email.
+      // If the email is unread, it should appear with a white background. If the email has been read, it should appear with a gray background.
+  });
+
 }
+
+// {
+//   "id": 100,
+//   "sender": "foo@example.com",
+//   "recipients": ["bar@example.com"],
+//   "subject": "Hello!",
+//   "body": "Hello, world!",
+//   "timestamp": "Jan 2 2020, 12:00 AM",
+//   "read": false,
+//   "archived": false
+// }
